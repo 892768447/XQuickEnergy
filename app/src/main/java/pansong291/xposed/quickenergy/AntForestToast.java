a9bee8d50ea9e21ff3e6b55d8d8c35035aab19dc
Irony <892768447@qq.com>
2019-12-24 15:48:47 +0800

    配置编译，增加中文汉化

--- app/src/main/java/pansong291/xposed/quickenergy/AntForestToast.java
+++ app/src/main/java/pansong291/xposed/quickenergy/AntForestToast.java
@@ -2,50 +2,41 @@ package pansong291.xposed.quickenergy;
 
 import android.content.Context;
 import android.widget.Toast;
+
 import pansong291.xposed.quickenergy.util.Config;
 import pansong291.xposed.quickenergy.util.Log;
 import pansong291.xposed.quickenergy.hook.XposedHook;
 
-public class AntForestToast
-{
- private static final String TAG = AntForestToast.class.getCanonicalName();
- public static Context context;
+public class AntForestToast {
+    private static final String TAG = AntForestToast.class.getCanonicalName();
+    public static Context context;
 
- public static void show(CharSequence cs)
- {
-  try
-  {
-   if(context != null && Config.showToast())
-   {
-    XposedHook.handler.post(
-     new Runnable()
-     {
-      CharSequence cs;
+    public static void show(CharSequence cs) {
+        try {
+            if (context != null && Config.showToast()) {
+                XposedHook.handler.post(
+                        new Runnable() {
+                            CharSequence cs;
 
-      public Runnable setData(CharSequence c)
-      {
-       cs = c;
-       return this;
-      }
+                            public Runnable setData(CharSequence c) {
+                                cs = c;
+                                return this;
+                            }
 
-      @Override
-      public void run()
-      {
-       try
-       {
-        Toast.makeText(context, cs, 0).show();
-       }catch(Throwable t)
-       {
-        Log.i(TAG, "show.run err:");
-        Log.printStackTrace(TAG, t);
-       }
-      }
-     }.setData(cs));
-   }
-  }catch(Throwable t)
-  {
-   Log.i(TAG, "show err:");
-   Log.printStackTrace(TAG, t);
-  }
- }
+                            @Override
+                            public void run() {
+                                try {
+                                    Toast.makeText(context, cs, 0).show();
+                                } catch (Throwable t) {
+                                    Log.i(TAG, "show.run err:");
+                                    Log.printStackTrace(TAG, t);
+                                }
+                            }
+                        }.setData(cs));
+            }
+        } catch (Throwable t) {
+            Log.i(TAG, "show err:");
+            Log.printStackTrace(TAG, t);
+        }
+    }
 }
